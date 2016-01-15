d97a40700f1e87461acd7a41a70619d5e3596329
Tomaz20 <ttomaz.200@gmail.com>
2016-01-15 18:51:22 +0000

    Pagina inicial

---
+++ Site/pages/inicio/scripts/estatisticas.js
@@ -0,0 +1,10 @@
+$(function(){
+
+	$("#nalunos").load("pages/inicio/scripts/nalunos.php");
+
+	$("#nprofs").load("pages/inicio/scripts/nprofs.php");
+
+	$("#ncursos").load("pages/inicio/scripts/ncursos.php");
+
+	$("#naudicoes").load("pages/inicio/scripts/naudicoes.php");
+});
