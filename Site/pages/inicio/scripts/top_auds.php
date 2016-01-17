ae76256e6e9a08f2ba73810aff924b83a326ba32
Tomaz20 <ttomaz.200@gmail.com>
2016-01-17 12:50:08 +0000

    mais coisas

--- Site/pages/inicio/scripts/top_auds.php
+++ Site/pages/inicio/scripts/top_auds.php
@@ -16,5 +16,12 @@
 		echo "<a href='pages/audicoes/consultar_audicao.html?id=".$aud['audicao_id']."' style='color:#666666;'>Audição dia ".$aud['audicao_data']." às ".$aud['audicao_hora'].", com o título <i>\"".$aud['audicao_titulo']." - ".$aud['audicao_subtitulo']."\"</i>, onde será(ão) apresentada(s) ".$nums['o']." obra(s) musical(is) ao longo de ".$nums['a']." atuação(ões)</a>";
 		echo "</th></tr>";
 	}
+	echo "<tr>
+		<th>
+			<a href='pages/audicoes/listar_audicoes.html'>
+                <button class='btn btn-outline btn-default btn-lg btn-block' type='button'>Ver Todas</button>
+            </a>
+        </th>
+    </tr>";
 
 ?>
